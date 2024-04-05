b6e0622d71fe86c373676e143344214f44d4b97e
D-SehKim <123012677+D-SehKim@users.noreply.github.com>
2024-04-05 01:53:01 -0400

    finished intro

--- scripts.js
+++ scripts.js
@@ -9,13 +9,5 @@ function typingEffect(element, text, delay) {
     }, delay);
 }
 
-function printContent() {
-    document.getElementById('content').style.display = 'grid';
-}
 
-window.transitionToPage = function(href) {
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
-}
+        
