d10b3f4cbaeac105c8c49ea1e9b03e0ef02f198e
Daniel Kim <123012677+D-SehKim@users.noreply.github.com>
2025-04-17 23:23:28 -0400

    added stuff

--- vite-react/src/GamePage2.jsx
+++ vite-react/src/GamePage2.jsx
@@ -1,23 +1,4 @@
-import { useEffect } from "react"
-
 function GamePage2() {
-    useEffect(() => {
-        const handleMessage = (event) => {
-        console.log("hello3")
-        if (event.data?.showPC) {
-            console.log('hello2')
-            document.getElementById('virtual-pc').style.display = "block"
-        }
-
-        if (event.data?.showPC == false) {
-            document.getElementById('virtual-pc').style.display = "none"
-        }
-        }
-
-        window.addEventListener("message", handleMessage)
-        return () => window.removeEventListener("message", handleMessage)
-    }, [])
-
     return (
         <div>
             <iframe
@@ -26,20 +7,6 @@ function GamePage2() {
             width="100%"
             height="100%"   
             style={{ border: 'none', position: 'fixed', top: 0, left: 0 }}
-            />
-
-            <iframe 
-            id="virtual-pc"
-            src="https://d-sehkim.github.io"
-            style={{
-                display: 'none',
-                position: 'absolute',
-                top: '3.5vh',
-                left: '15vw',
-                width: '70vw',
-                height: '87vh',
-                border: 'none'
-            }}
             />
         </div>
     )
